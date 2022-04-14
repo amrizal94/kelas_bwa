d1f93af2ec5f19615319ca7166c495c7ec85968e
amrizal94 <arimrizal94@gmail.com>
2022-04-14 12:07:34 +0700

    cozy splash_page

---
+++ cozy/lib/pages/splash_page.dart
@@ -0,0 +1,82 @@
+import 'package:cozy/theme.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      bottom: false,
+      child: Stack(
+        children: [
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Image.asset('assets/images/splash_image.png'),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 50,
+              left: 30,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 50,
+                  width: 50,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/logo.png'),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Text(
+                  'Find Cozy House\nto Stay and Happy',
+                  style: blackTextStyle.copyWith(fontSize: 24),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Stop membuang banyak waktu\npada tempat yang tidak habitable',
+                  style: greyTextStyle.copyWith(
+                    fontSize: 16,
+                  ),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                SizedBox(
+                  width: 210,
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(purpleColor),
+                      shape: MaterialStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(17),
+                        ),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      'Explore Now',
+                      style: whiteTextStyle.copyWith(
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    ));
+  }
+}
